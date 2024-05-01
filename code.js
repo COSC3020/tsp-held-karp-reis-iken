9f72f5fdde1d23845b87e91c81f4f92a0a74d0b4
reis-iken <144738295+reis-iken@users.noreply.github.com>
2024-05-01 13:59:44 -0600

    code.js

--- code.js
+++ code.js
@@ -1,3 +1,41 @@
 function tsp_hk(distance_matrix) {
-    return -1;
+    let n = distance_matrix.length;
+    let memo = {};
+
+    function getKey(cities, start) {
+        return Array.from(cities).join(',') + '-' + start;
+    }
+
+    function helper(cities, start) {
+        if (cities.size === 2) {
+            for (let city of cities) {
+                if (city !== start) {
+                    return distance_matrix[start][city];
+                }
+            }
+        } else {
+            let minimumDistance = Infinity;
+            for (let city of cities) {
+                if (city !== start) {
+                    let newCities = new Set(cities);
+                    newCities.delete(start);
+                    let dist = helper(newCities, city) + distance_matrix[start][city];
+                    minimumDistance = Math.min(minimumDistance, dist);
+                }
+            }
+            return minimumDistance;
+        }
+    }
+    let minimumDistance = Infinity;
+    for (let start = 1; start < n; start++) {
+        const cities = new Set();
+        for (let i = 0; i < n; i++) {
+            if (i !== start) {
+                cities.add(i);
+            }
+        }
+        const dist = helper(cities, start) + distance_matrix[start][0];
+        minimumDistance = Math.min(minimumDistance, dist);
+    }
+    return minimumDistance;
 }
